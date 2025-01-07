923b1b163008556c084d0a49fe9a4ce287d73d88
aolyang <aolyang@foxmail.com>
2025-01-07 17:26:15 +0800

    chore: first build

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as vue } from "./configs/vue"
+export { default as typescript } from "./configs/typescript"
+export { default as svelte } from "./configs/svelte"
+export { default as astro } from "./configs/astro"
